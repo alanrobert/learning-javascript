ee8bca1f7dbce00903497c867c3e01af493a8a0e
alanrobert <43940429+alanrobert@users.noreply.github.com>
2023-03-30 07:34:48 +0100

    Added new directory. Fixed Ace value on blackjack game :smile:

--- blackjack-game/main.js
+++ blackjack-game/main.js
@@ -69,8 +69,10 @@ function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13 ) + 1
     if (randomNumber > 10) {
         return 10 
-    } else if (randomNumber === 1) {
-        return 11 
+    } else if (randomNumber === 1 && sum < 11) {
+        return 11
+    } else if (randomNumber === 1 && sum > 11) {
+        return 1 
     } else {
         return randomNumber
     }
