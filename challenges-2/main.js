3498326b5819c7d41e33632244d72b712e328f62
alanrobert <43940429+alanrobert@users.noreply.github.com>
2023-03-30 06:44:20 +0100

    Finished challenges

--- challenges-2/main.js
+++ challenges-2/main.js
@@ -101,7 +101,7 @@ let weekday = "Friday"
 if (dayOfMonth === 13 && weekday === "Friday") {
     console.log("😱") 
 } else {
-    console.log("Your Safe")
+    console.log("Your Safe") 
 }
 
 
@@ -165,9 +165,40 @@ orderFruit()
 
 // 9. Random password generator (solo-challenge)
 
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let generateEl = document.getElementById("generatePassword")
+let passwordOneEl = document.getElementById("passwordOne")
+let passwordTwoEl = document.getElementById("passwordTwo")
+
+let passwordLength = 12
+
+function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+
+function generateRandomPassword() {
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomCharacter()
+    }
+    return randomPassword
+}
+
+const generatedPasswordOne = generateRandomPassword()
+const generatedPasswordTwo = generateRandomPassword()
+
+generateEl.addEventListener("click", function() {
+    passwordOneEl.textContent = generatedPasswordOne
+    passwordTwoEl.textContent = generatedPasswordTwo
+})
 
 
 
+
+console.log(generatedPasswordOne)
 
 
 
