234f11846cfd12e202e8f32ab38c24b5e8c65c22
Al <43940429+alanrobert@users.noreply.github.com>
2023-04-18 21:07:34 +0100

    added for loop
    got value from input and put in array

--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -1,4 +1,21 @@
 
-function saveInput() {
-    console.log("lead-saved")
-}
+// let = "can be re-assigned"
+// const = "short for constant", "can't be re-assigned"
+// if possible, use const. If not, use let.
+
+let inputBtn = document.getElementById("input-btn")
+let inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+let myLeads = []
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    for (let i = 0; i < myLeads.length; i++) {
+        ulEl.textContent = myLeads
+    }
+})
+
+
+
+
+
