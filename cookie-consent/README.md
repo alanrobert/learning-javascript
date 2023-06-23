24d4d4332fbed32b08e557b2c1416d74f5a441f3
Al <43940429+alanrobert@users.noreply.github.com>
2023-06-23 07:20:37 +0100

    added README

---
+++ cookie-consent/README.md
@@ -0,0 +1,19 @@
+# What was studied in this module
+
+## setTimeout - Control when pop up appeared
+
+## element.style - To dynamically update an elements css properties
+
+## html forms - Input types of text, email and password
+
+## formData & .get() - Get data from form as an object then the get method to access that data to use in app
+
+## event.preventDefault - To get more control over forms
+
+## CSS:row-reverse - flexbox
+
+## ('mouseenter) & ('mouseover) - using add.EventListener method
+
+## element.classList.toggle - Toggling class on and off
+
+## 'disabled' attribute - To block users from clicking the exit button. Also setTimeout on button to make clickable after set time. 
--- cookie-consent/index.js
+++ cookie-consent/index.js
@@ -2,6 +2,8 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const choiceBtns = document.getElementById('modal-choice-btns')
 
 // setTimeout(function() {
 //     modal.style.display = 'inline'
@@ -11,6 +13,33 @@ modal.style.display = 'inline'
 modalCloseBtn.addEventListener('click', function() {
     modal.style.display = 'none'
 })
+
+/*   
+Challenge: 
+1. Take control of the decline btn. (What do you
+   need to do to make that possible?)
+2. Set up an eventListener that is triggered 
+   when a user's cursor hovers over the decline 
+   button. (Google the event to listen out for!)
+3. For now, just log out "hovered" when that 
+   happens. 
+*/ 
+
+
+
+declineBtn.addEventListener('mouseenter', function() {
+    choiceBtns.classList.toggle('reverse')
+})
+
+/*   
+Challenge: 
+1. Take control of the div holding the buttons.
+2. In index.css, set up a selector for a new class
+   that uses flex-direction to reverse the order 
+   of its child elements.
+3. Toggle that class on the div holding the buttons 
+   when a user's cursor hovers over the decline button.
+*/ 
 
 consentForm.addEventListener('submit', function(e) {
     e.preventDefault()
@@ -28,6 +57,13 @@ consentForm.addEventListener('submit', function(e) {
         document.getElementById('uploadText').textContent = "Making the sale..."
     }, 1500)
 
+/*
+    Challenge: 
+1. Make the button that closes the modal disabled.
+2. Make that button become usable when the final 
+   modal message has been displayed to the user.
+*/ 
+
     setTimeout(function() {
         document.getElementById('modal-inner').innerHTML = 
         `<h2>Thanks <span class="modal-display-name">${consentFormName}</span>, you sucker! </h2>
@@ -36,7 +72,8 @@ consentForm.addEventListener('submit', function(e) {
             <img src="img/pirate.gif">
         </div>
         ` 
-    }, 3000)
+        modalCloseBtn.disabled = false
+    }, 3000)            
 })
 
 
